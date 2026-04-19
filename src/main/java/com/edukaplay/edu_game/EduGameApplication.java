6fe94b56d29539726beb2356a1375b625b8c0e13
Xinito <axbacosta@gmail.com>
2026-04-19 13:15:26 -0500

    Commit inicial: Edugame Spring Boot 4.0.5 con Java 25 y PostgreSQL

---
+++ src/main/java/com/edukaplay/edu_game/EduGameApplication.java
@@ -0,0 +1,13 @@
+package com.edukaplay.edu_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edukaplay/edu_game/EduGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edukaplay.edu_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
